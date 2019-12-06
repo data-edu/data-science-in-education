55395a2bed110490309b88da95d2e284718c3e2d
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2019-12-06 07:50:44 -0500

    generating citations for packages

---
+++ generate-citations.R
@@ -0,0 +1,6 @@
+# for packages
+
+knitr::write_bib(c("bookdown", "tidyverse", "dplyr", "tidyr", 
+                 "ggplot2", "sjPlot", "lme4", "ggraph", "tidygraph", 
+                 "caret", "readxl", "here", "lubridate", "dummies",
+                 "janitor", "dataedu"), "packages.bib")
