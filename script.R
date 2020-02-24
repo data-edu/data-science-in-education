7863dfcda3fcf81a7d2b0afa2ce8e8a3be26b9e0
Isabella Velásquez <ivelasq@gmail.com>
2020-02-23 22:26:25 -0800

    create before chapter script

---
+++ script.R
@@ -0,0 +1,12 @@
+rm(list=ls(all=TRUE))
+
+options(digits = 3)
+
+knitr::opts_chunk$set(
+    comment = "#>",
+    cache = TRUE,
+    out.width = "100%",
+    fig.align = "center",
+    warning = FALSE,
+    dpi = 300
+)
