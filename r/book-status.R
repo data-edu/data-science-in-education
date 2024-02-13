eb9b5bbbd28d5773a1d17dfe6cb6e34b6f6038a2
Isabella Velasquez <ivelasq@gmail.com>
2024-02-12 20:14:34 -0800

    GitHub Actions attempt 3

--- r/book-status.R
+++ r/book-status.R
@@ -1,18 +1,18 @@
-# book-status.R
-
-library(tidyverse)
-library(cloc)
-library(wordcountaddin)
-
-# Calculating word count
-output <- tibble(file = list.files()) %>% 
-    filter(str_detect(file, ".Rmd")) %>% 
-    mutate(file = as.character(file)) %>% 
-    mutate(wordcount = map_int(unlist(.), word_count))
-
-output %>% 
-    summarize(total_wordcount = sum(wordcount))
-
-cloc::cloc_by_file(getwd()) %>% 
-    filter(language == "Markdown") %>% 
-    summarize(total_loc = sum(loc))
+# # book-status.R
+# 
+# library(tidyverse)
+# library(cloc)
+# library(wordcountaddin)
+# 
+# # Calculating word count
+# output <- tibble(file = list.files()) %>% 
+#     filter(str_detect(file, ".Rmd")) %>% 
+#     mutate(file = as.character(file)) %>% 
+#     mutate(wordcount = map_int(unlist(.), word_count))
+# 
+# output %>% 
+#     summarize(total_wordcount = sum(wordcount))
+# 
+# cloc::cloc_by_file(getwd()) %>% 
+#     filter(language == "Markdown") %>% 
+#     summarize(total_loc = sum(loc))
